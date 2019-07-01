3817d2969ccd5c9052e4cd2ea1ca9e639f723538
Matican Matican <maticangroup@gmail.com>
2019-07-01 16:57:14 +0430

    Hossein: form model class parser

---
+++ ModelSerializer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hossein
+ * Date: 01/07/19
+ * Time: 16:53
+ */
+
+namespace App\FormModels;
+
+
+class ModelSerializer
+{
+
+    /**
+     * ModelSerializer constructor.
+     * @param $json
+     * @param $class
+     */
+    public function __construct($json, $class)
+    {
+        $properties = json_encode($json);
+        $classInstance = new $class();
+        foreach ($properties as $property => $value) {
+            $setterName = "set" . $property;
+            $classInstance->{$setterName}($value);
+        }
+        return $classInstance;
+    }
+}
