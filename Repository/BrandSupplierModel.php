ea563491b47dcfb559bef399c503ef008ef156ab
Matican Matican <maticangroup@gmail.com>
2019-07-04 09:55:49 +0430

    Amirhossein change brand supplier model

--- Repository/BrandSupplierModel.php
+++ Repository/BrandSupplierModel.php
@@ -11,108 +11,23 @@ namespace App\FormModels\Repository;
 
 class BrandSupplierModel
 {
-    private $id;
-    private $name;
-    private $phone;
-    private $address;
-    private $contactPersonId;
-    private $contactPersonName;
+    private $companyId;
     private $brandId;
 
     /**
      * @return mixed
      */
-    public function getId()
+    public function getCompanyId()
     {
-        return $this->id;
+        return $this->companyId;
     }
 
     /**
-     * @param mixed $id
+     * @param mixed $companyId
      */
-    public function setId($id): void
+    public function setCompanyId($companyId): void
     {
-        $this->id = $id;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param mixed $name
-     */
-    public function setName($name): void
-    {
-        $this->name = $name;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPhone()
-    {
-        return $this->phone;
-    }
-
-    /**
-     * @param mixed $phone
-     */
-    public function setPhone($phone): void
-    {
-        $this->phone = $phone;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getAddress()
-    {
-        return $this->address;
-    }
-
-    /**
-     * @param mixed $address
-     */
-    public function setAddress($address): void
-    {
-        $this->address = $address;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getContactPersonId()
-    {
-        return $this->contactPersonId;
-    }
-
-    /**
-     * @param mixed $contactPersonId
-     */
-    public function setContactPersonId($contactPersonId): void
-    {
-        $this->contactPersonId = $contactPersonId;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getContactPersonName()
-    {
-        return $this->contactPersonName;
-    }
-
-    /**
-     * @param mixed $contactPersonName
-     */
-    public function setContactPersonName($contactPersonName): void
-    {
-        $this->contactPersonName = $contactPersonName;
+        $this->companyId = $companyId;
     }
 
     /**
