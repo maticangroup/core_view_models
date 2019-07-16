6a707c7e9d88b91a7450dc7c90b31e42741e0287
Matican Matican <maticangroup@gmail.com>
2019-07-16 12:06:11 +0430

    Amirhossein

---
+++ Delivery/DispatchPaymentModel.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Amirhossein
+ * Date: 7/16/2019
+ * Time: 12:02 PM
+ */
+
+namespace App\FormModels\Delivery;
+
+
+class DispatchPaymentModel
+{
+    private $dispatchPaymentId;
+    private $dispatchPaymentName;
+    private $dispatchPaymentAmount;
+    private $dispatchPaymentStatus;
+    private $dispatchId;
+    private $deliveryPersonId;
+
+    /**
+     * @return mixed
+     */
+    public function getDispatchPaymentId()
+    {
+        return $this->dispatchPaymentId;
+    }
+
+    /**
+     * @param mixed $dispatchPaymentId
+     */
+    public function setDispatchPaymentId($dispatchPaymentId): void
+    {
+        $this->dispatchPaymentId = $dispatchPaymentId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDispatchPaymentName()
+    {
+        return $this->dispatchPaymentName;
+    }
+
+    /**
+     * @param mixed $dispatchPaymentName
+     */
+    public function setDispatchPaymentName($dispatchPaymentName): void
+    {
+        $this->dispatchPaymentName = $dispatchPaymentName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDispatchPaymentAmount()
+    {
+        return $this->dispatchPaymentAmount;
+    }
+
+    /**
+     * @param mixed $dispatchPaymentAmount
+     */
+    public function setDispatchPaymentAmount($dispatchPaymentAmount): void
+    {
+        $this->dispatchPaymentAmount = $dispatchPaymentAmount;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDispatchPaymentStatus()
+    {
+        return $this->dispatchPaymentStatus;
+    }
+
+    /**
+     * @param mixed $dispatchPaymentStatus
+     */
+    public function setDispatchPaymentStatus($dispatchPaymentStatus): void
+    {
+        $this->dispatchPaymentStatus = $dispatchPaymentStatus;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDispatchId()
+    {
+        return $this->dispatchId;
+    }
+
+    /**
+     * @param mixed $dispatchId
+     */
+    public function setDispatchId($dispatchId): void
+    {
+        $this->dispatchId = $dispatchId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDeliveryPersonId()
+    {
+        return $this->deliveryPersonId;
+    }
+
+    /**
+     * @param mixed $deliveryPersonId
+     */
+    public function setDeliveryPersonId($deliveryPersonId): void
+    {
+        $this->deliveryPersonId = $deliveryPersonId;
+    }
+}
---
+++ Delivery/DispatchPaymentStatusModel.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Amirhossein
+ * Date: 7/16/2019
+ * Time: 12:05 PM
+ */
+
+namespace App\FormModels\Delivery;
+
+
+class DispatchPaymentStatusModel
+{
+    private $dispatchPaymentStatusId;
+    private $dispatchPaymentStatusName;
+    private $dispatchPaymentStatusMachineName;
+    private $dispatchPaymentStatusColor;
+    private $dispatchPaymentId;
+
+    /**
+     * @return mixed
+     */
+    public function getDispatchPaymentStatusId()
+    {
+        return $this->dispatchPaymentStatusId;
+    }
+
+    /**
+     * @param mixed $dispatchPaymentStatusId
+     */
+    public function setDispatchPaymentStatusId($dispatchPaymentStatusId): void
+    {
+        $this->dispatchPaymentStatusId = $dispatchPaymentStatusId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDispatchPaymentStatusName()
+    {
+        return $this->dispatchPaymentStatusName;
+    }
+
+    /**
+     * @param mixed $dispatchPaymentStatusName
+     */
+    public function setDispatchPaymentStatusName($dispatchPaymentStatusName): void
+    {
+        $this->dispatchPaymentStatusName = $dispatchPaymentStatusName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDispatchPaymentStatusMachineName()
+    {
+        return $this->dispatchPaymentStatusMachineName;
+    }
+
+    /**
+     * @param mixed $dispatchPaymentStatusMachineName
+     */
+    public function setDispatchPaymentStatusMachineName($dispatchPaymentStatusMachineName): void
+    {
+        $this->dispatchPaymentStatusMachineName = $dispatchPaymentStatusMachineName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDispatchPaymentStatusColor()
+    {
+        return $this->dispatchPaymentStatusColor;
+    }
+
+    /**
+     * @param mixed $dispatchPaymentStatusColor
+     */
+    public function setDispatchPaymentStatusColor($dispatchPaymentStatusColor): void
+    {
+        $this->dispatchPaymentStatusColor = $dispatchPaymentStatusColor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDispatchPaymentId()
+    {
+        return $this->dispatchPaymentId;
+    }
+
+    /**
+     * @param mixed $dispatchPaymentId
+     */
+    public function setDispatchPaymentId($dispatchPaymentId): void
+    {
+        $this->dispatchPaymentId = $dispatchPaymentId;
+    }
+}
