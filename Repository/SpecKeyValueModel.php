f6f9c4d49f64427f57b52f9c6322ca7d2aa9fa9a
Matican Matican <maticangroup@gmail.com>
2019-07-21 17:47:15 +0430

    Amirhossein

---
+++ Repository/SpecKeyValueModel.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Amirhossein
+ * Date: 7/21/2019
+ * Time: 5:46 PM
+ */
+
+namespace App\FormModels\Repository;
+
+
+class SpecKeyValueModel
+{
+    private $itemId;
+    private $keyId;
+    private $value;
+
+    /**
+     * @return mixed
+     */
+    public function getItemId()
+    {
+        return $this->itemId;
+    }
+
+    /**
+     * @param mixed $itemId
+     */
+    public function setItemId($itemId): void
+    {
+        $this->itemId = $itemId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getKeyId()
+    {
+        return $this->keyId;
+    }
+
+    /**
+     * @param mixed $keyId
+     */
+    public function setKeyId($keyId): void
+    {
+        $this->keyId = $keyId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param mixed $value
+     */
+    public function setValue($value): void
+    {
+        $this->value = $value;
+    }
+}
