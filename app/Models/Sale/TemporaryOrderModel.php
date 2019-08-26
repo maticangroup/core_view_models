8c9d54cfb2a0051272d31a78a0a79aa62b64e226
Matican Matican <maticangroup@gmail.com>
2019-08-26 13:00:08 +0430

    hossein, adding temporary order model

---
+++ app/Models/Sale/TemporaryOrderModel.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hossein
+ * Date: 26/08/19
+ * Time: 12:35
+ */
+
+namespace Matican\Models\Sale;
+
+
+class TemporaryOrderModel
+{
+    private $temporaryOrderLocationID;
+    private $temporaryOrderDeliveryMethodID;
+    private $temporaryOrderDeliveryQueueID;
+
+    /**
+     * @return mixed
+     */
+    public function getTemporaryOrderLocationID()
+    {
+        return $this->temporaryOrderLocationID;
+    }
+
+    /**
+     * @param mixed $temporaryOrderLocationID
+     */
+    public function setTemporaryOrderLocationID($temporaryOrderLocationID): void
+    {
+        $this->temporaryOrderLocationID = $temporaryOrderLocationID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTemporaryOrderDeliveryMethodID()
+    {
+        return $this->temporaryOrderDeliveryMethodID;
+    }
+
+    /**
+     * @param mixed $temporaryOrderDeliveryMethodID
+     */
+    public function setTemporaryOrderDeliveryMethodID($temporaryOrderDeliveryMethodID): void
+    {
+        $this->temporaryOrderDeliveryMethodID = $temporaryOrderDeliveryMethodID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTemporaryOrderDeliveryQueueID()
+    {
+        return $this->temporaryOrderDeliveryQueueID;
+    }
+
+    /**
+     * @param mixed $temporaryOrderDeliveryQueueID
+     */
+    public function setTemporaryOrderDeliveryQueueID($temporaryOrderDeliveryQueueID): void
+    {
+        $this->temporaryOrderDeliveryQueueID = $temporaryOrderDeliveryQueueID;
+    }
+
+
+}
