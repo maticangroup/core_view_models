ed1dbd2ee29304d3600cfdcbcd31286c70df4f00
Matican Matican <maticangroup@gmail.com>
2019-11-14 15:33:00 +0330

    hossein, adding district model

---
+++ app/Models/Repository/DistrictModel.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hossein
+ * Date: 14/11/19
+ * Time: 15:31
+ */
+
+namespace Matican\Models\Repository;
+
+
+class DistrictModel
+{
+    private $cityId;
+    private $districtId;
+    private $districtName;
+}
