6a1413d4019f9aa63482cab86ccbf6a1ad568120
Matican Matican <maticangroup@gmail.com>
2019-11-14 15:54:39 +0330

    hossein, fixing district model getter and setters

--- app/Models/Repository/DistrictModel.php
+++ app/Models/Repository/DistrictModel.php
@@ -14,4 +14,53 @@ class DistrictModel
     private $cityId;
     private $districtId;
     private $districtName;
+
+    /**
+     * @return mixed
+     */
+    public function getCityId()
+    {
+        return $this->cityId;
+    }
+
+    /**
+     * @param mixed $cityId
+     */
+    public function setCityId($cityId): void
+    {
+        $this->cityId = $cityId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDistrictId()
+    {
+        return $this->districtId;
+    }
+
+    /**
+     * @param mixed $districtId
+     */
+    public function setDistrictId($districtId): void
+    {
+        $this->districtId = $districtId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDistrictName()
+    {
+        return $this->districtName;
+    }
+
+    /**
+     * @param mixed $districtName
+     */
+    public function setDistrictName($districtName): void
+    {
+        $this->districtName = $districtName;
+    }
+
 }
